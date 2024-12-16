53a436b5557f6722fd28431dc7f5c528d1245ec0
Thomas Brüggemann <thomas.brueggemann@maersk.com>
2024-12-16 07:44:52 +0100

    new vactrol profiles

--- Vactrols.cpp
+++ Vactrols.cpp
@@ -28,8 +28,11 @@ void Vactrols::ApplyPreset(Preset &preset)
 
 void Vactrols::SetBass(int value)
 {
-	auto mappedValueA = map(1023 - value, 0, 1023, 0, 102);
-	auto mappedValueB = map(value, 0, 1023, 0, 100);
+	int countA = (sizeof(bass_a) / sizeof(bass_a[0])) - 1;
+	auto mappedValueA = map(1023 - value, 0, 1023, 0, countA);
+
+	int countB = (sizeof(bass_b) / sizeof(bass_b[0])) - 1;
+	auto mappedValueB = map(value, 0, 1023, 0, countB);
 
 	bassDAC.setVoltageA(bass_a[mappedValueA]);
 	bassDAC.setVoltageB(bass_b[mappedValueB]);
@@ -39,8 +42,11 @@ void Vactrols::SetBass(int value)
 
 void Vactrols::SetMiddle(int value)
 {
-	auto mappedValueA = map(1023 - value, 0, 1023, 0, 97);
-	auto mappedValueB = map(value, 0, 1023, 0, 96);
+	int countA = (sizeof(mid_a) / sizeof(mid_a[0])) - 1;
+	auto mappedValueA = map(1023 - value, 0, 1023, 0, countA);
+
+	int countB = (sizeof(mid_b) / sizeof(mid_b[0])) - 1;
+	auto mappedValueB = map(value, 0, 1023, 0, countB);
 
 	midDAC.setVoltageA(mid_a[mappedValueA]);
 	midDAC.setVoltageB(mid_b[mappedValueB]);
@@ -50,8 +56,11 @@ void Vactrols::SetMiddle(int value)
 
 void Vactrols::SetTreble(int value)
 {
-	auto mappedValueA = map(1023 - value, 0, 1023, 0, 84);
-	auto mappedValueB = map(value, 0, 1023, 0, 84);
+	int countA = (sizeof(treble_a) / sizeof(treble_a[0])) - 1;
+	auto mappedValueA = map(1023 - value, 0, 1023, 0, countA);
+
+	int countB = (sizeof(treble_b) / sizeof(treble_b[0])) - 1;
+	auto mappedValueB = map(value, 0, 1023, 0, countB);
 
 	trebleDAC.setVoltageA(treble_a[mappedValueA]);
 	trebleDAC.setVoltageB(treble_b[mappedValueB]);
@@ -61,10 +70,8 @@ void Vactrols::SetTreble(int value)
 
 void Vactrols::SetVolume(int value)
 {
-	auto mappedValueA = map(value, 0, 1023, 0, 109);
-
-  D_println(mappedValueA);
-
+	int countA = (sizeof(volume_a) / sizeof(volume_a[0])) - 1;
+	auto mappedValueA = map(value, 0, 1023, 0, countA);
 	volumeDAC.setVoltageA(volume_a[mappedValueA]);
 
 	volumeDAC.updateDAC();
